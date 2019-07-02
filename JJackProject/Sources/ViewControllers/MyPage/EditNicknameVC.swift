2175e6fa37e31f11587c330d1b4e50c0fee49774
모상일 <mosang1230@gmail.com>
2019-07-02 13:41:59 +0900

    CREATE - Editting Nicname View

---
+++ JJackProject/Sources/ViewControllers/MyPage/EditNicknameVC.swift
@@ -0,0 +1,33 @@
+//
+//  EditNicknameVC.swift
+//  JJackProject
+//
+//  Created by SangIl Mo on 02/07/2019.
+//  Copyright © 2019 SangIl Mo. All rights reserved.
+//
+
+import UIKit
+
+class EditNicknameVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func Back(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+}
