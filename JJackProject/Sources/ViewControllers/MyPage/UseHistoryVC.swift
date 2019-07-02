2c6476d59df82193ac9cd2908c1eb61dc51fe649
모상일 <mosang1230@gmail.com>
2019-07-02 17:21:33 +0900

    CREATING - History view

---
+++ JJackProject/Sources/ViewControllers/MyPage/UseHistoryVC.swift
@@ -0,0 +1,30 @@
+//
+//  UseHistoryVC.swift
+//  JJackProject
+//
+//  Created by SangIl Mo on 02/07/2019.
+//  Copyright © 2019 SangIl Mo. All rights reserved.
+//
+
+import UIKit
+
+class UseHistoryVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
